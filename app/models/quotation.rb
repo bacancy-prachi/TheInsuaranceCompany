22946fe1c937884f760d82d5135977ac16f19ec0
bacancy-prachi <prachi.jain@bacancy.com>
2022-07-28 18:16:24 +0530

    minor changes in text field

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -8,4 +8,5 @@ class Quotation < ApplicationRecord
 	accepts_nested_attributes_for :quotation_transaction, :property_legal_description, :property_information, :note, :request_callback, :premium_calculation,  allow_destroy: true
 
 	validates_presence_of :email, :first_name, :last_name, :address, :postal_code, :city, :phone, :province, :municiple_evaluation_of_property
+	# validates :phone, numericality: { only_integer: true }
 end
